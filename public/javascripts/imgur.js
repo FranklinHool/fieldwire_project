7bbdd28a2aa71dde483c7068311c3a7efb6a9d86
Franklin <fhool@uci.edu>
2017-08-17 15:24:00 -0700

    lazy loading, drag and drop, overall optimizations

--- public/javascripts/imgur.js
+++ public/javascripts/imgur.js
@@ -1,32 +1,33 @@
 /**
  * Created by Franklin on 8/11/2017.
  */
-var app = angular.module('fieldwire', ['infinite-scroll', 'FBAngular'] );
+var app = angular.module('fieldwire', ['infinite-scroll', 'FBAngular', 'lvl.directives.dragdrop', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-lazy-loader' ] );
 
-app.controller('imageController', function ($scope, $http, Fullscreen) {
+app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal, $log) {
 
     $scope.images = [];
+    $scope.cart = [];
     var count = 1;
     //Submit button refresh
-    $scope.start = function () {
-        $scope.images = [];
-        count = 1;
-        console.log($scope.searchText);
-        $http({
-            method: "GET",
-            url: 'https://api.imgur.com/3/gallery/search/1?q=' + $scope.searchText,
-            headers: {Authorization: 'Client-ID 4071f54a38b614d'},
-            json: true
-        }).then(function success(response) {
-            for (var i = 0; i < response.data.data.length; i++) {
-                $scope.images.push(response.data.data[i]);
-            }
-            console.log(response);
-            count++;
-        }, function error(response) {
-            response.statusText;
-        })
-    }
+    // $scope.start = function () {
+    //     $scope.images = [];
+    //     count = 1;
+    //     console.log($scope.searchText);
+    //     $http({
+    //         method: "GET",
+    //         url: 'https://api.imgur.com/3/gallery/search/1?q=' + $scope.searchText,
+    //         headers: {Authorization: 'Client-ID 4071f54a38b614d'},
+    //         json: true
+    //     }).then(function success(response) {
+    //         for (var i = 0; i < response.data.data.length; i++) {
+    //             $scope.images.push(response.data.data[i]);
+    //         }
+    //         console.log(response);
+    //         count++;
+    //     }, function error(response) {
+    //         response.statusText;
+    //     })
+    // }
     //Scroll down refresh
     $scope.newPage = function () {
         $http({
@@ -49,7 +50,7 @@ app.controller('imageController', function ($scope, $http, Fullscreen) {
         $scope.images = [];
         $http({
             method: "GET",
-            url: 'https://api.imgur.com/3/gallery/search/0?q=' + $scope.searchText,
+            url: 'https://api.imgur.com/3/gallery/search/0?q=' + $scope.searchText + '&q_type=png',
             headers: {Authorization: 'Client-ID 5a84da5f70cb884'},
             json: true
         }).then(function success(response) {
@@ -69,7 +70,44 @@ app.controller('imageController', function ($scope, $http, Fullscreen) {
     $scope.getURL = function (i) {
         return (i.is_album) ? 'https://imgur.com/' + i.cover + '.jpg' : 'https://i.imgur.com/' + i.id + '.jpg';
     }
+
+
+    $scope.dropped = function(dragEl, dropEl) {
+        for(var i = 0; i < $scope.images.length; i += 1){
+            var result = $scope.images[i];
+            if(result.id === dragEl){
+                //$scope.images.splice(i,1);
+                $scope.cart.push(result);
+            }
+        }
+        console.log("The element " + dragEl + " has been dropped on " + dropEl + "!");
+    };
+
+    $scope.animationsEnabled = true;
+    $scope.open = function () {
+        var modalInstance = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            controllerAs: 'scope'
+        });
+
+        modalInstance.result.then(function () {
+            console.log($scope.cart);
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
 })
+
+app.controller('ModalInstanceCtrl', function ($uibModalInstance, $scope) {
+    $scope.ok = function () {
+        console.log($scope.cart);
+        $uibModalInstance.dismiss();
+    };
+
+});
+
 
 
 
