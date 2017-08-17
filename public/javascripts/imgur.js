ea240beb01bf9b30167a3d8c4ab8a6bca30f69a4
Franklin <fhool@uci.edu>
2017-08-17 16:27:54 -0700

    more optimization

--- public/javascripts/imgur.js
+++ public/javascripts/imgur.js
@@ -1,13 +1,16 @@
 /**
  * Created by Franklin on 8/11/2017.
  */
-var app = angular.module('fieldwire', ['infinite-scroll', 'FBAngular', 'lvl.directives.dragdrop', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-lazy-loader' ] );
+var app = angular.module('fieldwire', ['infinite-scroll', 'FBAngular', 'lvl.directives.dragdrop', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-lazy-loader']);
 
 app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal, $log) {
 
-    $scope.images = [];
+    var images = [];
+    $scope.imagesView = [];
     $scope.cart = [];
+    var load = true;
     var count = 1;
+    var set = 0;
     //Submit button refresh
     // $scope.start = function () {
     //     $scope.images = [];
@@ -30,24 +33,41 @@ app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal
     // }
     //Scroll down refresh
     $scope.newPage = function () {
-        $http({
-            method: "GET",
-            url: 'https://api.imgur.com/3/gallery/search/' + count + '?q=' + $scope.searchText,
-            headers: {Authorization: 'Client-ID 5a84da5f70cb884'},
-            json: true
-        }).then(function success(response) {
-            for (var i = 0; i < response.data.data.length; i++) {
-                $scope.images.push(response.data.data[i]);
+         if (load == true) {
+             console.log("HERE");
+            $http({
+                method: "GET",
+                url: 'https://api.imgur.com/3/gallery/search/' + count + '?q=' + $scope.searchText,
+                headers: {Authorization: 'Client-ID 5a84da5f70cb884'},
+                json: true
+            }).then(function success(response) {
+                for (var i = 0; i < response.data.data.length; i++) {
+                    images.push(response.data.data[i]);
+                    if(i<25){
+                        $scope.imagesView.push(response.data.data[i]);
+                    }
+                }
+                count++;
+                set++;
+                load = !load;
+            }, function error(response) {
+                response.statusText;
+            })
+        } else {
+            for (var i = 0; i < 25; i++) {
+                $scope.imagesView.push(images[i + 25 * set])
             }
-            count++;
-        }, function error(response) {
-            response.statusText;
-        })
+            load = !load;
+        }
+
+
     }
 
     //real-time refresh
     $scope.refresh = function () {
-        $scope.images = [];
+        load = false;
+        images = [];
+        $scope.imagesView = [];
         $http({
             method: "GET",
             url: 'https://api.imgur.com/3/gallery/search/0?q=' + $scope.searchText + '&q_type=png',
@@ -55,14 +75,19 @@ app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal
             json: true
         }).then(function success(response) {
             for (var i = 0; i < response.data.data.length; i++) {
-                $scope.images.push(response.data.data[i]);
+                if(i<25){
+                    $scope.imagesView.push(response.data.data[i]);
+                }
+                images.push(response.data.data[i]);
             }
             count++;
+            set++;
+            load = !load;
         }, function error(response) {
             response.statusText;
         })
     }
-    
+
     $scope.fullscreen = function (i) {
         Fullscreen.enable(document.getElementById(i.id));
     }
@@ -72,10 +97,10 @@ app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal
     }
 
 
-    $scope.dropped = function(dragEl, dropEl) {
-        for(var i = 0; i < $scope.images.length; i += 1){
-            var result = $scope.images[i];
-            if(result.id === dragEl){
+    $scope.dropped = function (dragEl, dropEl) {
+        for (var i = 0; i < $scope.imagesView.length; i += 1) {
+            var result = $scope.imagesView[i];
+            if (result.id === dragEl) {
                 //$scope.images.splice(i,1);
                 $scope.cart.push(result);
             }
