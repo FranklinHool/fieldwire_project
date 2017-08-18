27dddf2f15e107c064761d6f40596012571bf175
Franklin <fhool@uci.edu>
2017-08-17 19:33:57 -0700

    fixed extra boxes

--- public/javascripts/imgur.js
+++ public/javascripts/imgur.js
@@ -9,13 +9,14 @@ app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal
     $scope.imagesView = [];
     $scope.cart = [];
     var load = true;
-    var count = 1;
+    var count = 0;
     var lastIndex = 0;
 
+    //Scrolling search function
     $scope.newPage = function () {
 
-         if (load == true ) {
-             console.log("HERE");
+        if (load == true) {
+            console.log("HERE");
             $http({
                 method: "GET",
                 url: 'https://api.imgur.com/3/gallery/search/' + count + '?q=' + $scope.searchText,
@@ -24,29 +25,25 @@ app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal
             }).then(function success(response) {
                 for (var i = 0; i < response.data.data.length; i++) {
                     images.push(response.data.data[i]);
-                    if(i<15){
-                        $scope.imagesView.push(response.data.data[i]);
-                        lastIndex++;
-                    }
+                }
+                for(var j=0;j<response.data.data.length/2;j++){
+                    $scope.imagesView.push(images[lastIndex]);
+                    lastIndex++;
                 }
                 count++;
                 load = !load;
-                console.log("Images: " + images.length);
-                console.log("images Viewed: " + $scope.imagesView.length);
+                console.log("Last Index: " + lastIndex);
             }, function error(response) {
                 response.statusText;
             })
         } else {
-            for (var i = 0; i < 15; i++) {
-                $scope.imagesView.push(images[i + lastIndex])
-                console.log("HERE");
+            for (var i = 0; i < 25; i++) {
+                $scope.imagesView.push(images[lastIndex])
+                lastIndex++;
             }
-            lastIndex+=15;
+            console.log("Last Index: " + lastIndex);
             load = !load;
-             console.log(images);
-
-             console.log("Images: " + images.length);
-             console.log("images Viewed: " + $scope.imagesView.length);
+            console.log(images);
         }
 
 
@@ -65,35 +62,35 @@ app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal
             json: true
         }).then(function success(response) {
             for (var i = 0; i < response.data.data.length; i++) {
-                if(i<15){
-                    $scope.imagesView.push(response.data.data[i]);
-                    lastIndex++;
-                }
                 images.push(response.data.data[i]);
+        }
+            for(var j=0;j<response.data.data.length/2;j++){
+                $scope.imagesView.push(images[lastIndex]);
+                lastIndex++;
             }
             count++;
             load = !load;
-            console.log("Images: " + images.length);
-            console.log("images Viewed: " + $scope.imagesView.length);
+            console.log("LAST INDEX: " + lastIndex);
         }, function error(response) {
             response.statusText;
         })
     }
 
+    //Allows fullscreen on click of photo
     $scope.fullscreen = function (i) {
         Fullscreen.enable(document.getElementById(i.id));
     }
 
+    //Gets the URL of the image
     $scope.getURL = function (i) {
         return (i.is_album) ? 'https://imgur.com/' + i.cover + '.png' : 'https://i.imgur.com/' + i.id + '.png';
     }
 
-
+    //Drag and Drop logic
     $scope.dropped = function (dragEl, dropEl) {
         for (var i = 0; i < $scope.imagesView.length; i += 1) {
             var result = $scope.imagesView[i];
             if (result.id === dragEl) {
-                //$scope.images.splice(i,1);
                 $scope.cart.push(result);
             }
         }
@@ -101,6 +98,7 @@ app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal
         console.log($scope.cart);
     };
 
+    //Modal controller for data movement and opening
     $scope.animationsEnabled = true;
     $scope.open = function () {
         var modalInstance = $uibModal.open({
@@ -108,7 +106,7 @@ app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal
             templateUrl: 'myModalContent.html',
             controller: 'ModalInstanceCtrl',
             resolve: {
-                items: function(){
+                items: function () {
                     return $scope.cart;
                 }
             }
@@ -122,6 +120,7 @@ app.controller('imageController', function ($scope, $http, Fullscreen, $uibModal
     };
 })
 
+//Controller for pop up modal. Controls the cart data.
 app.controller('ModalInstanceCtrl', function ($uibModalInstance, items, $scope, Fullscreen) {
     $scope.items = items;
     console.log(items);
@@ -138,8 +137,8 @@ app.controller('ModalInstanceCtrl', function ($uibModalInstance, items, $scope, 
         Fullscreen.enable(document.getElementById(i.id));
     }
 
-    $scope.remove = function(i){
-        $scope.items.splice(i,1);
+    $scope.remove = function (i) {
+        $scope.items.splice(i, 1);
     }
 });
 
